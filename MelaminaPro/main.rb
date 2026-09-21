a0712a55ee22927761df0003a04b6ff33ab9289a
madha241194-eng <madha241194@gmail.com>
2026-09-21 23:24:02 +0530

    Update main.rb

--- MelaminaPro/main.rb
+++ MelaminaPro/main.rb
@@ -1,51 +1,80 @@
 module MelaminaPro
 
+  def self.add_board(entities, x, y, z, width, depth, height)
+    points = [
+      [x, y, z],
+      [x + width, y, z],
+      [x + width, y + depth, z],
+      [x, y + depth, z]
+    ]
+
+    face = entities.add_face(points)
+    face.pushpull(height)
+  end
+
   def self.create_cabinet
     model = Sketchup.active_model
 
     model.start_operation("Melamina Pro Cabinet", true)
 
-    entities = model.active_entities
+    group = model.active_entities.add_group
+    group.name = "Melamina Cabinet"
 
-    # Cabinet dimensions in mm
+    entities = group.entities
+
+    # Cabinet dimensions
     width  = 800.mm
     height = 720.mm
     depth  = 560.mm
     board  = 18.mm
 
-    # Create cabinet group
-    group = entities.add_group
-    group.name = "Melamina Cabinet"
-
-    cabinet = group.entities
-
     # Left panel
-    left = cabinet.add_cube(
-      Geom::Point3d.new(0, 0, 0),
-      board, depth, height
+    add_board(
+      entities,
+      0,
+      0,
+      0,
+      board,
+      depth,
+      height
     )
 
     # Right panel
-    right = cabinet.add_cube(
-      Geom::Point3d.new(width - board, 0, 0),
-      board, depth, height
+    add_board(
+      entities,
+      width - board,
+      0,
+      0,
+      board,
+      depth,
+      height
     )
 
     # Bottom
-    bottom = cabinet.add_cube(
-      Geom::Point3d.new(board, 0, 0),
-      width - board * 2, depth, board
+    add_board(
+      entities,
+      board,
+      0,
+      0,
+      width - board * 2,
+      depth,
+      board
     )
 
     # Top
-    top = cabinet.add_cube(
-      Geom::Point3d.new(board, 0, height - board),
-      width - board * 2, depth, board
+    add_board(
+      entities,
+      board,
+      0,
+      height - board,
+      width - board * 2,
+      depth,
+      board
     )
 
     model.commit_operation
 
-    UI.messagebox("Melamina Cabinet Created!")
+    UI.messagebox("Melamina Pro Cabinet Created!")
   end
 
 end
